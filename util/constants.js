7834d4f4f3cec06169854789a97211b12b454c73
hieuminh65 <hieuminhnguyen@usf.edu>
2024-02-02 11:35:15 -0500

    Add token removal and storage functionality Update authentication state and token handling

--- util/constants.js
+++ util/constants.js
@@ -11,31 +11,3 @@ export const SCREEN_NAMES = {
   MESSAGE: "Message",
   AUDIO_PLAYER: "Audio Screen",
 };
-
-/* ERRORS */
-export const SOUND_PLAYER_ERR = {
-  play: "Error playing sound",
-  pause: "Error pausing sound",
-  seek: "Error seeking sound",
-  unload: "Error unloading sound",
-  load: `Sorry, I can't load this audio now, try again later`,
-};
-
-export const RECORDER_ERR = {
-  time_limit: "1 Minute Limit Reached, Audio has finished recording.",
-  start_rec: "Failed to start recording.",
-  stop_rec: "Failed to stop recording.",
-  permission: "Recording permission not granted",
-};
-
-export const ERROR_AI = {
-  RECORDING_NOT_FOUND: "Couldn't find any recording. Please try again.",
-  SEND: "Failed to send your recording. Please try again.",
-};
-
-export const ERROR_ENCODING = "Error encoding audio to base64";
-export const GENERAL_ERROR = "Sorry! Something went wrong";
-export const CONNECTION_ERROR = "Please check your connection!";
-/* ERRORS */
-
-export const AWS = "";
