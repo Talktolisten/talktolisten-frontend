2c723645698fc3a506614cc82a75f0ae60411508
hieuminh65 <hieuminhnguyen@usf.edu>
2024-02-24 22:53:38 -0500

    Add enableScreens() to improve performance

--- App.js
+++ App.js
@@ -2,14 +2,15 @@
 import { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Provider } from 'react-redux';
+import { enableScreens } from 'react-native-screens';
 import store from './redux/store';
 import { loadFonts } from './util/helpers';
 import Nav from './navigation/Nav';
 
 
 export default function App() {
+  enableScreens();
   const [fontsLoaded, setFontsLoaded] = useState(false);
-
   useEffect(() => {
     const loadFontData = async () => {
       await loadFonts();
